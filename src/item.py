9fc319c9209e3b59f41d967c4dcc550a613e7829
Данила <spook9400@mail.ru>
2023-07-21 14:14:41 +0300

    homework-3 remake

--- src/item.py
+++ src/item.py
@@ -22,6 +22,17 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f'{self.__name}'
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них')
+        return self.quantity + other.quantity
+
     @property
     def name(self):
         return self.__name
@@ -36,7 +47,6 @@ class Item:
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,4 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""#
+"""Здесь надо написать тесты с использованием pytest для модуля item."""  #
 from src.item import Item
 
 
@@ -14,16 +14,39 @@ def test_calculate_total_prices():
 
     # применяем скидку
 
+
 def test_apply_discount():
     item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
 
     item1.apply_discount()
 
     assert item1.price == 8000.0
+    assert item2.price == 20000.0
+
 
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
 
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.__repr__() == Item("Смартфон", 10000, 20)
+    assert item2.__repr__() == Item("Ноутбук", 20000, 5)
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1.__str__() == "Сматрфон"
+    assert item2.__str__() == "Ноутбук"
+
+
+def test_add():
+    assert Item.
 ##
